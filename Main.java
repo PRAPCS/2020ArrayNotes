a6ee2a87a4a0790e3a407dea56d9387f69c2a411
Mr. Burger <bburger@d155.org>
2020-12-09 19:01:16 +0000

    Added Code

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Declare an Array
+    int[] arr1 = new int[10];
+    System.out.println("The array has " + arr1.length + " slots");
+    // Defaults all slots to 0
+    arr1[1] = 50;
+    arr1[arr1.length - 1] = 100;
+    arr1[0] = arr1[1] + arr1[arr1.length - 1];
+    arr1[2]++;
+    arr1[1]--;
+    // Print the contents of the Array
+    for (int i = 0; i < arr1.length; i++) {
+      System.out.println(arr1[i]);
+    }
+    System.out.println("======================");
+    for (int i = 0; i < arr1.length; i++) {
+      arr1[i] = i;
+    }
+
+    for (int i = 0; i < arr1.length; i++) {
+      System.out.println(arr1[i]);
+    }
+System.out.println("======================");
+    int[] arr2 = { 4, 5, 6, 7, 8, 9, 10 };
+    for (int i = 0; i < arr2.length; i++) {
+      System.out.println(arr2[i]);
+    }
+
   }
 }
